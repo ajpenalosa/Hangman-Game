3a84cb5518cc1189a835349c938fd0507df09dcd
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2017-12-03 19:55:46 -0800

    Updated code to be more organized Added code so that once number of correctly guessed letters is equal to number of letters in current word adds 1 to wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,110 @@
-// Creates an array of Pixar Characters
-var pixarCharacters = ["Flik","Princess Atta","Hopper","Merida","King Fergus","Queen Elinor","Lightning McQueen","Doc Hudson","Sally Carrera","Nemo","Dory","Marlin","Joy","Sadness","Anger","James P Sullivan","Mike Wazowski","Boo","Mr Incredible","Elastigirl","Edna Mode","Buzz Lightyear","Sheriff Woody","Bo Peep","Carl Fredricksen","Ellie Fredricksen","Russell"];
+//------------------------------------------------------------
+// ---------- VARIABLES
+//------------------------------------------------------------
 
-// Creates an array of the alphabet
-var alphabet = [];
-
-for (var i = 65; i <= 90; i++) {
-    alphabet[alphabet.length] = String.fromCharCode(i);
-}
-console.log(alphabet);
-
-for (var i = 0; i < alphabet.length; i++) {
-    // Logs individual letters of the current word to the console
-    console.log("The character at index " + [i] + " is: " + alphabet[i]);
-}
-
-// Chooses a random Pixar character name from the array
-var randomCharacter = pixarCharacters[Math.floor(Math.random() * pixarCharacters.length)];
-
-// Changes letters to uppercase
-var upperCaseWord = randomCharacter.toUpperCase();
-
-// Logs the chosen Pixar character name to the console
-console.log(upperCaseWord);
-
-// Creating variables
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 10;
-var lettersMatched = 0;
+var gameStarted = false;
+// var lettersMatched = 0; // MIGHT NOT NEED THIS
 
-// Empty array to hold letters of the current word
+// MESSAGES
+
+
+
+//------------------------------------------------------------
+// ---------- ARRAYS
+//------------------------------------------------------------
+
+// Creates an array of Pixar Characters
+var pixarCharacters = ["Flik","Princess Atta","Hopper","Merida","King Fergus","Queen Elinor","Lightning McQueen","Doc Hudson","Sally Carrera","Nemo","Dory","Marlin","Joy","Sadness","Anger","James P Sullivan","Mike Wazowski","Boo","Mr Incredible","Elastigirl","Edna Mode","Buzz Lightyear","Sheriff Woody","Bo Peep","Carl Fredricksen","Ellie Fredricksen","Russell"];
+
+//------------------------------------------------------------
+// ---------- EMPTY ARRAYS
+//------------------------------------------------------------
+
+var alphabet = [];
 var currentWord = [];
+var correctGuesses = [];
+var wrongGuesses = [];
 
-// Grabbing elements by ID
+//------------------------------------------------------------
+// ---------- GRAB ELEMENTS BY ID
+//------------------------------------------------------------
+
 document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
 document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
 
-// Puts underscores as placeholders for each letter of the current word
-for (var i = 0; i < upperCaseWord.length; i++) {
+//------------------------------------------------------------
+// ---------- LOGIC
+//------------------------------------------------------------
 
-    if (upperCaseWord[i] === " ") {
-        currentWord[i] = " ";
-    } else {
-        currentWord[i] = "_";
-    }
-
-    var placeHolder = currentWord.join("");
-    document.getElementById("current-word").innerHTML = placeHolder;
-
-    // Logs individual letters of the current word to the console
-    console.log("The character at index " + [i] + " is: " + upperCaseWord[i]);
+// Creates alphabet array
+for (var i = 65; i <= 90; i++) {
+    alphabet[alphabet.length] = String.fromCharCode(i);
 };
 
-// Gives JavaScript a function to execute when onkeyup event fires
-document.onkeyup = function(event) {
+// Logs individual letters of the ALPHABET to the console
+// for (var i = 0; i < alphabet.length; i++) {
+//     console.log("The character at index " + [i] + " is: " + alphabet[i]);
+// };
 
-    // Determines which key was pressed
-    var userGuess = event.key.toUpperCase();
+function newGame() {
 
-    // Assigns correctly guessed letters to the appropriate place in the current word
-    for (var j = 0; j < upperCaseWord.length; j++) {
-
-        if (upperCaseWord[j] === userGuess) {
-            currentWord[j] = userGuess;
-            lettersMatched++;
-            console.log("Letters matched: " + lettersMatched);
+    gameStarted = true;
+    
+    // Chooses a random Pixar character name from the array
+    var randomCharacter = pixarCharacters[Math.floor(Math.random() * pixarCharacters.length)];
+    
+    // Changes letters to uppercase
+    var upperCaseWord = randomCharacter.toUpperCase();
+    
+    // Puts underscores as placeholders for each letter of the current word
+    for (var i = 0; i < upperCaseWord.length; i++) {
+    
+        if (upperCaseWord[i] === " ") {
+            currentWord[i] = " ";
+            // Push empty space to correctGuesses array
+            correctGuesses.push(" ");
         } else {
-            guessesRemaining--;
+            currentWord[i] = "_";
+        }
+    
+        var placeHolder = currentWord.join("");
+        document.getElementById("current-word").innerHTML = placeHolder;
+    
+    };
+    
+    document.onkeyup = function(event) {
+        
+        // Determines which key was pressed
+        var userGuess = event.key.toUpperCase();
+    
+        // Assigns correctly guessed letters to the appropriate place in the current word
+        for (var j = 0; j < upperCaseWord.length; j++) {
+    
+            if (upperCaseWord[j] === userGuess) {
+                currentWord[j] = userGuess;
+                
+                // adds letter to correctGuesses array
+                correctGuesses.push(userGuess);
+            }
         }
 
-        if (lettersMatched === upperCaseWord.length) {
+        if (correctGuesses.length === currentWord.length) {
             wins++;
-            lettersMatched = 0;
-            console.log("Wins: " + wins);
         }
 
-    }
-    document.getElementById("user-guesses").innerHTML = event.key;
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
-    document.getElementById("current-word").innerHTML = currentWord.join("");
+        console.log(correctGuesses);
+        document.getElementById("user-guesses").innerHTML = event.key;
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+        document.getElementById("current-word").innerHTML = currentWord.join("");
+    };
+    
+    //---------- CONSOLE ---------
+    // Logs the chosen Pixar character name to the console
+    console.log(upperCaseWord);
+
 };
